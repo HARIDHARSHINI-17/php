c29f6153fb4dcb41e799017d409327733a8628ab
Haridharshini.S <94096479+HARIDHARSHINI-17@users.noreply.github.com>
2022-04-28 09:30:47 +0530

    Update default checking.php

--- default checking.php
+++ default checking.php
@@ -1,14 +1,4 @@
 <?php
-
-//function concat($str1, $str2, $delimiter)
-//{
-    //return $str1 . $delimiter . $str2;
-//}
-
-//$message = concat('Hi', 'there!', ' ');
-
-//echo $message;
-//
 function concat($str1,$str2=" ",$str3){
     return $str1.$str2.$str3;
 }

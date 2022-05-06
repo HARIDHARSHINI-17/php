dbc721e8e2685740f737fc92cd73296ebb57b9f2
Haridharshini.S <94096479+HARIDHARSHINI-17@users.noreply.github.com>
2022-05-06 16:51:34 +0530

    Add files via upload

---
+++ magicConstants.php/magic constant.php
@@ -0,0 +1,4 @@
+ <?php   
+        echo "<h3>Example for __LINE__</h3>";     
+        echo "You are at line number " . __LINE__ . "<br><br>";  
+?>  
---
+++ magicConstants.php/magicConstantTrait.php
@@ -0,0 +1,8 @@
+<?php   
+    echo "<h3>Example for __TRAIT__</h3>";    
+    trait trait_new {    
+        function one(){        
+            echo __TRAIT__; 
+        }    
+    }    
+?>

59ded34a789b4c4d99d04af66b7c6207f9187102
Haridharshini.S <94096479+HARIDHARSHINI-17@users.noreply.github.com>
2022-05-12 14:42:23 +0530

    Update interface example.php

--- oops.php/interface example.php
+++ oops.php/interface example.php
@@ -3,7 +3,7 @@ interface fruit{
     public function banana();
     public function apple(){
         echo "I LIKE APPLE";
-    }
+    }//throws error
 }
 class fruitname implements fruit{
     public function banana(){
